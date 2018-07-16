e2fdf43a778a320cbeb4ae8edd187660c5d5645f
ferhat <ferhat.bedir@hotmail.com>
2018-07-16 14:22:34 +0300

    Email Service App

--- src/main/java/com/ferhatbedir/apply_for_a_job/mailinfo/MailInfo.java
+++ src/main/java/com/ferhatbedir/apply_for_a_job/mailinfo/MailInfo.java
@@ -2,6 +2,6 @@ package com.ferhatbedir.apply_for_a_job.mailinfo;
 
 public class MailInfo {
 
-    public static String HR_MAIL_ADDRESS = "ferhat.bedr@gmail.com";
-    public static String HR_MAIL_PASSWORD = "Fb1231992.";
+    public static String HR_MAIL_ADDRESS = "Your_Email";
+    public static String HR_MAIL_PASSWORD = "Your_Password";
 }
